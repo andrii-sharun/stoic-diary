523983a6f9439586e567cb3f3312c641b69a372e
Andrii Sharun <andrii.sharun.dev@gmail.com>
2021-10-03 16:21:17 +0300

    Updated webpack config to resole .svg

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ export default {
       {
         test: /\.s[ac]ss$/,
         use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.svg/,
+        type: 'asset/inline'
       }
     ]
   },
