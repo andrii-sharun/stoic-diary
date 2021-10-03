d41dbc58d3272c22bb69f1e95f5ef586bd7d4303
Andrii Sharun <andrii.sharun.dev@gmail.com>
2021-10-03 19:13:45 +0300

    Updated LoginLayout with Login

--- web/client/src/component/Layout/LoginLayout/LoginLayout.tsx
+++ web/client/src/component/Layout/LoginLayout/LoginLayout.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react'
+import Login from '../../Login/Login'
 import './LoginLayout.scss'
 
 const LoginLayout: FC = () => {
   return (
     <div className='login__layout'>
-      <h1>Login layout</h1>
+      <Login />
     </div>
   )
 }
--- web/test/client/component/Layout/LoginLayout.test.tsx
+++ web/test/client/component/Layout/LoginLayout.test.tsx
@@ -1,11 +1,13 @@
 import { render, screen } from '@testing-library/react'
+jest.mock('@components/Login/Login', () => () => (<h1>Login Component</h1>))
 import LoginLayout from '../../../../client/src/component/Layout/LoginLayout/LoginLayout'
+
 
 describe('LoginLayout', () => {
   it('should render component', () => {
     render(<LoginLayout />)
-    const text = screen.getByText('Login layout')
+    const heading = screen.getByRole('heading', { name: 'Login Component' })
 
-    expect(text).toBeInTheDocument()
+    expect(heading).toBeInTheDocument()
   })
 })
