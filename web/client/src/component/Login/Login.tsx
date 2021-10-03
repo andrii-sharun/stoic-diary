3a08791fd4245141ddfa63a4bd929fd34e1ff9d1
Andrii Sharun <andrii.sharun.dev@gmail.com>
2021-10-03 19:02:08 +0300

    Updated Login with Inputs

--- web/client/src/component/Login/Login.tsx
+++ web/client/src/component/Login/Login.tsx
@@ -1,11 +1,44 @@
-import { FC } from 'react'
+import { FC, useState, ChangeEventHandler } from 'react'
+import Form from '../Form/Form'
+import Input from '../Input/Input'
 import './Login.scss'
 import LoginHeading from './LoginHeading/LoginHeading'
 
 const Login: FC = () => {
+  const [emailValue, setEmailValue] = useState<string>('')
+  const [passwordlValue, setPasswordValue] = useState<string>('')
+
+  const handleChangeEmail: ChangeEventHandler<HTMLInputElement> = (e): void => {
+    setEmailValue(e.target.value)
+  }
+
+  const handleChangePassword: ChangeEventHandler<HTMLInputElement> = (e): void => {
+    setPasswordValue(e.target.value)
+  }
+
   return (
     <div className='login'>
       <LoginHeading text='Stoic Diary' />
+      <Form className='login__form'>
+        <div className="login__inputs">
+          <Input
+            className='login__input'
+            name='email'
+            type='email'
+            plaсeholder='Enter your email'
+            value={emailValue}
+            onChange={handleChangeEmail}
+          />
+          <Input
+            className='login__input'
+            name='password'
+            type='password'
+            plaсeholder='Enter your password'
+            value={passwordlValue}
+            onChange={handleChangePassword}
+          />
+        </div>
+      </Form>
     </div>
   )
 }
--- web/test/client/component/Login/Login.test.tsx
+++ web/test/client/component/Login/Login.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Login from '../../../../client/src/component/Login/Login'
 
 describe('Login', () => {
@@ -9,5 +10,31 @@ describe('Login', () => {
   it('should render component with heading', () => {
     const heading = screen.getByRole('heading', { name: 'Stoic Diary' })
     expect(heading).toBeInTheDocument()
+  })
+
+  it('should render component with email input', () => {
+    const emailInput = screen.getByPlaceholderText('Enter your email')
+    expect(emailInput).toBeInTheDocument()
+  })
+
+  it('should change value of email input', () => {
+    const emailInput = screen.getByPlaceholderText('Enter your email')
+    const emailValue = 'e@mail.com'
+
+    userEvent.type(emailInput, emailValue)
+    expect(emailInput).toHaveValue(emailValue)
+  })
+
+  it('should render component with password input', () => {
+    const passwordInput = screen.getByPlaceholderText('Enter your password')
+    expect(passwordInput).toBeInTheDocument()
+  })
+
+  it('should change value of password input', () => {
+    const passwordInput = screen.getByPlaceholderText('Enter your password')
+    const passwordValue = 'qwerty123'
+
+    userEvent.type(passwordInput, passwordValue)
+    expect(passwordInput).toHaveValue(passwordValue)
   })
 })
